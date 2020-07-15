e88295bc22a47a7f755eedb38cd2b7de9cab5c3c
jude okoroafor <judeokoroafor@gmail.com>
2020-07-15 07:26:17 +0100

    Removed unused files

--- javascript/script.js
+++ javascript/script.js
@@ -169,6 +169,14 @@ var x = setInterval(function() {
   // If the count down is over, write some text 
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
+    document.getElementById("demo").innerHTML = "TIMER EXPIRED";
   }
 }, 1000);
+
+
+
+// Loading
+
+$(function() {
+	$(".loading-wrapper").fadeOut(5000);
+});
