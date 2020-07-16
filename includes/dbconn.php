5d2d7e90db2258e4a95630e0ec7797e9f11926f0
jude okoroafor <judeokoroafor@gmail.com>
2020-07-16 08:34:19 +0100

    Set up live dB credentials

--- includes/dbconn.php
+++ includes/dbconn.php
@@ -5,7 +5,7 @@
 if(!empty($_POST["send"])) {
 	$email = $_POST["userEmail"];
 
-	$conn = mysqli_connect("localhost", "root", "", "scan-tc_db") or die("Connection Error: database currently set up locally" . mysqli_error($conn));
+	$conn = mysqli_connect("us-cdbr-east-02.cleardb.com", "b89ae53a6851cf", "a4034734", "heroku_d9f93aa84a0eee6") or die("Connection Error: database currently set up locally" . mysqli_error($conn));	
 	mysqli_query($conn, "INSERT INTO subscribers (email) VALUES ('" . $email. "')");
 }
 ?>
